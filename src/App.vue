6fd552f5d3c5fa66edecc1dbed88a95ee01f9a00
Ville Zuo <villezuo@gmail.com>
2023-06-08 17:22:02 +0800

    use mediapipe

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,91 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Greet from "./components/Greet.vue";
+import Greet from "./components/Greet.vue"
+import { Camera } from "@mediapipe/camera_utils"
+import { ControlPanel, DropDownControl, FPS, FireFn, ControlPanelListener, Slider, SourcePicker, SourceType, StaticText, Toggle } from "@mediapipe/control_utils"
+import { clamp, drawConnectors, drawLandmarks, drawRectangle, lerp } from "@mediapipe/drawing_utils"
+import { Holistic, POSE_CONNECTIONS, FACEMESH_TESSELATION, HAND_CONNECTIONS } from "@mediapipe/holistic"
+import { ref, onMounted } from "vue"
+
+const videoElement = ref<HTMLVideoElement>()
+const canvasElement = ref<HTMLCanvasElement>()
+
+onMounted(() => {
+
+  const canv = canvasElement.value as HTMLCanvasElement
+  const canvasCtx = canv.getContext('2d') as CanvasRenderingContext2D;
+
+  function onResults(results: any) {
+    canvasCtx.save();
+
+    const width = canvasElement.value?.width as number
+    const height = canvasElement.value?.height as number
+    canvasCtx.clearRect(0, 0, width, height);
+    canvasCtx.drawImage(results.segmentationMask, 0, 0, width, height);
+
+    // Only overwrite existing pixels.
+    canvasCtx.globalCompositeOperation = 'source-in';
+    canvasCtx.fillStyle = '#00FF00';
+    canvasCtx.fillRect(0, 0, width, height);
+
+    // Only overwrite missing pixels.
+    canvasCtx.globalCompositeOperation = 'destination-atop';
+    canvasCtx.drawImage(
+      results.image, 0, 0, width, height);
+
+    canvasCtx.globalCompositeOperation = 'source-over';
+    drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
+      { color: '#00FF00', lineWidth: 4 });
+    drawLandmarks(canvasCtx, results.poseLandmarks,
+      { color: '#FF0000', lineWidth: 2 });
+    drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_TESSELATION,
+      { color: '#C0C0C070', lineWidth: 1 });
+    drawConnectors(canvasCtx, results.leftHandLandmarks, HAND_CONNECTIONS,
+      { color: '#CC0000', lineWidth: 5 });
+    drawLandmarks(canvasCtx, results.leftHandLandmarks,
+      { color: '#00FF00', lineWidth: 2 });
+    drawConnectors(canvasCtx, results.rightHandLandmarks, HAND_CONNECTIONS,
+      { color: '#00CC00', lineWidth: 5 });
+    drawLandmarks(canvasCtx, results.rightHandLandmarks,
+      { color: '#FF0000', lineWidth: 2 });
+    canvasCtx.restore();
+  }
+
+  const holistic = new Holistic({
+    locateFile: (file) => {
+      return `/@mediapipe/holistic/${file}`;
+    }
+  });
+  holistic.setOptions({
+    modelComplexity: 1,
+    smoothLandmarks: true,
+    enableSegmentation: true,
+    smoothSegmentation: true,
+    refineFaceLandmarks: true,
+    minDetectionConfidence: 0.5,
+    minTrackingConfidence: 0.5
+  });
+  holistic.onResults(onResults);
+
+  const camera = new Camera(videoElement.value as HTMLVideoElement, {
+    onFrame: async () => {
+      await holistic.send({ image: videoElement.value as HTMLVideoElement });
+    },
+    width: 1280,
+    height: 720
+  });
+  camera.start();
+})
+
 </script>
 
 <template>
   <div class="container">
+
+    <video class="input_video" ref="videoElement"></video>
+    <canvas class="output_canvas" ref="canvasElement" :width=1280 :height=720></canvas>
+
     <h1>Welcome to Tauri!</h1>
 
     <div class="row">
@@ -28,13 +108,9 @@ import Greet from "./components/Greet.vue";
       +
       <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
       +
-      <a href="https://github.com/tauri-apps/tauri-vscode" target="_blank"
-        >Tauri</a
-      >
+      <a href="https://github.com/tauri-apps/tauri-vscode" target="_blank">Tauri</a>
       +
-      <a href="https://github.com/rust-lang/rust-analyzer" target="_blank"
-        >rust-analyzer</a
-      >
+      <a href="https://github.com/rust-lang/rust-analyzer" target="_blank">rust-analyzer</a>
     </p>
 
     <Greet />
